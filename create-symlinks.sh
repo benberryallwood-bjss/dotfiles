e14bc1eaf0820999c89b3d82368ba3e24ffde909
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-10-11 18:25:44 +0100

    Add line to create symlink for .gitconfig

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Git
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
 # Neovim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
