098621c7d84f8150a4c4a540c2afc4b832efdf1e
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-10-12 16:41:24 +0100

    Add installation of vim-plug and neovim plugins to install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,10 @@
 
 # Then bundle
 brew bundle --file ~/.dotfiles/Brewfile
+
+# Install vim-plug
+sh -c 'curl -fLo ~/.config/local/share/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install neovim plugins
+nvim -c PlugInstall
