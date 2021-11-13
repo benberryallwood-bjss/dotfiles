831f7d1317f0dff880b1b7d782e07a321c111d0d
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-13 17:44:10 +0000

    Create and link tmux.conf for plugins

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -13,3 +13,6 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -s ~/.dotfiles/nvim/.html-template.html ~/.config/nvim/.html-template.html
+
+# tmux
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
